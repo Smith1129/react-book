9d80642e34f5c99ee08a301f44f94baf5ef0d5d3
zzz <1006472680@qq.com>
2019-05-25 18:04:55 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,50 +1,21 @@
-import React, {Component, Fragment} from 'react'
-import './App.css'
-import { CSSTransition,TransitionGroup } from 'react-transition-group';
-
-class App extends Component{
-    constructor(props)
-    {
+import React,{ Component,Fragment } from 'react'
+import {Globalstyle} from "./style";
+import Header from './common/header/index'
+class App extends Component {
+    constructor(props) {
         super(props)
-        this.state = {
-            show:true,
-            list:[]
-        }
-        this.handleChangeState = this.handleChangeState.bind(this)
+        // this.state
     }
+
     render() {
-        return(
+        return (
         <Fragment>
-            <TransitionGroup>
-                {this.state.list.map((item,index)=>{
-                    return(
-                        <CSSTransition
-                            // in={this.state.show} //绑定数据
-                            timeout={1000} //动画时间
-                            classNames={'fade'}
-                            unmountOnExit   //隐藏的时候把dom也取消了
-                            onEntered = {(el) => {              //动画结束执行的钩子函数
-                                el.style.color = 'blue'
-                            }}
-                            appear={true} //第一次展示的时候就显示动画 对应的className .fade-appear .fade-appear-active .fade-appear-done
-                        >
-                        <div key={index}>{item}</div>
-                        </CSSTransition>
-                    )
-                })
-                }
-                {/*<span className={this.state.show ? 'show' : 'hide'}>hello</span>*/}
-            </TransitionGroup>
-                <button type='button' onClick={this.handleChangeState}>toggle</button>
+        <Globalstyle/>
+        <Header/>
         </Fragment>
         )
     }
-    handleChangeState(){
-        this.setState((prevState)=>{
-            return {
-                list:[...prevState.list,'item']
-            }
-        })
-    }
 }
+
 export default App
+
--- src/store/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const CHANGE_INPUT_VALUE = 'change-inputValue'
-export const CHANGE_LIST = 'change-list'
-export const CHANGE_ADDLIST = 'change-addList'
-
